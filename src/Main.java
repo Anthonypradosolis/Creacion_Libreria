c4f96c602863e89a18f33c18b832aef169eb24c0
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-02-16 11:28:56 +0100

    Ejercicio Calculadora

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Main {
+    /**
+     *
+     * @param args escribimos los valores, y luego visualizamos el resultado
+     */
+    public static void main(String[] args) {
+        Float retorno = Operaciones_Basicas.operaciones(Operaciones_Basicas.SUMA,5,5);
+        if(retorno == null)
+            System.out.println("Algo fallo");
+        else
+            System.out.println("El resultado es: "+retorno);
+    }
+}
---
+++ src/Operaciones_Basicas.java
@@ -0,0 +1,39 @@
+public class Operaciones_Basicas {
+        private float num;
+        private float num2;
+        public static final int SUMA=1;
+        public static final int RESTA=2;
+        public static final int MULTIPLICACION=3;
+        public static final int DIVISION=4;
+        public Operaciones_Basicas(float num, float num2) {
+            this.num = num;
+            this.num2= num2;
+        }
+    /**
+     *
+     * @param a tipo de operacion
+     * @param num primer numero para operar
+     * @param num2 segundo numero para operar
+     * @return resultado de las operaciones
+     */
+        public static Float operaciones(int a,float num,float num2){
+            float resultado=0;
+            switch (a){
+                case SUMA:
+                    resultado=num+num2;
+                    break;
+                case RESTA:
+                    resultado=num-num2;
+                    break;
+                case MULTIPLICACION:
+                    resultado=num*num2;
+                    break;
+                case DIVISION:
+                    resultado=num/num2;
+                    break;
+                default:
+                    System.out.println("Operacion no Valida");
+            }
+            return  resultado;
+        }
+}
