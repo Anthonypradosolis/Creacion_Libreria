be907af649129585a91042dac758d55095fec56b
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-03-11 09:31:04 +0100

    comprobacion

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
      * @param args escribimos los valores, y luego visualizamos el resultado
      */
     public static void main(String[] args) {
-        Float retorno = Operaciones_Basicas.operaciones(Operaciones_Basicas.SUMA,5,5);
+        Float retorno = Operaciones_Basicas.operaciones(Operaciones_Basicas.RAIZ,5,5);
         if(retorno == null)
             System.out.println("Algo fallo");
         else
